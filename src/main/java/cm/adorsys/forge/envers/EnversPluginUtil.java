425adacfe0ea3644a8f3e669fc09d5de8949ffc6
clovis.gakam <clovis.gakam@gmail.com>
2013-12-29 17:39:42 +0100

    ...

--- src/main/java/cm/adorsys/forge/envers/EnversPluginUtil.java
+++ src/main/java/cm/adorsys/forge/envers/EnversPluginUtil.java
@@ -1,5 +1,6 @@
 package cm.adorsys.forge.envers;
 
+import java.io.File;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -17,7 +18,7 @@ import org.jboss.forge.shell.ShellMessages;
 import org.jboss.forge.shell.plugins.PipeOut;
 
 public class EnversPluginUtil {
-	
+
 	public Node getPropertyWithName(String name,FileResource<?> resource){
 		Node matchedNode = null ;
 		Node resource2 = XMLParser.parse(resource.getResourceInputStream());
@@ -38,27 +39,30 @@ public class EnversPluginUtil {
 	}
 
 	public void auditEntity(JavaClass javaClass) {
-		if(!javaClass.hasAnnotation(Audited.class)){
-			javaClass.addImport(Audited.class);
-			javaClass.addAnnotation(Audited.class);
-		}	
+		if(javaClass.hasAnnotation(Audited.class))throw new IllegalStateException("The element '"
+				+ javaClass.getName() + "' is already annotated with @"
+				+ Audited.class.getSimpleName());
+		javaClass.addImport(Audited.class);
+		javaClass.addAnnotation(Audited.class);
 	}
 
 	public void auditField(Field<JavaClass> field){
-		if(!field.hasAnnotation(Audited.class)){
-			field.getOrigin().addImport(Audited.class);
-			field.addAnnotation(Audited.class);
-		}
+		if(field.hasAnnotation(Audited.class))throw new IllegalStateException("The element '"
+				+ field.getName() + "' is already annotated with @"
+				+ Audited.class.getSimpleName());
+		field.getOrigin().addImport(Audited.class);
+		field.addAnnotation(Audited.class);
 	}
-	
+
 	public void setupDefaultProperties(PipeOut out,Project project)
 	{
 		FileResource<?> persistencceXml;
 		ResourceFacet resourceFacet = project.getFacet(ResourceFacet.class);
 		persistencceXml = resourceFacet.getResource("META-INF/persistence.xml");
+		File underlyingResourceObject = persistencceXml.getUnderlyingResourceObject();
 		for (int i = 0; i < 5; i++) {
 		}
 		ShellMessages.success(out, "default envers persistence xml properties are configure correctly.");
 	}
-	
+
 }
